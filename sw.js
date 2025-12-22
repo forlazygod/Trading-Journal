5a444d302f8a9170fe2091d923a6374e1e9dbab1
forlazygod <forlazygod@gmail.com>
2025-12-22 19:39:55 +0700

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'trading-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'trade-journal-v1';
+const assets = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
